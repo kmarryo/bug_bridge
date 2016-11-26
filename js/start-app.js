81282301c44d3f923d4a7e22ed45ff3cc73557ed
kmarryo <mario.s.lemke@gmail.com>
2016-11-26 16:48:02 +0100

    Update readme and clean up code

--- js/start-app.js
+++ js/start-app.js
@@ -1,14 +1,27 @@
+// This hides the overlays for losing or winning the game from the beginning
 $(".overlay").hide();
 $(".overlay-congrats").hide();
 
-$.ajaxPrefilter(function (options, originalOptions, jqXHR) {
+// After the player chose a character and hits play, the game will load via ajax
+$.ajaxPrefilter(function (options) {
     options.async = true;
 });
 
 $("#chars img").click(function () {
-    $("#chars img.active").removeClass("active")
-    $(this).addClass("active");
-
+    $("#chars img.active").removeClass("active");
+    $(this).addClass("active clicked");
+    var _char = $(this).data("char");
+    if (_char === "boy") {
+        chooseChar = chars.boy;
+    } else if (_char === "cat-girl") {
+        chooseChar = chars.catGirl;
+    } else if (_char === "horn-girl") {
+        chooseChar = chars.hornGirl;
+    } else if (_char === "pink-girl") {
+        chooseChar = chars.pinkGirl;
+    } else {
+        chooseChar = chars.princessGirl;
+    }
 });
 
 $(".start").click(function () {
@@ -23,28 +36,13 @@ $(".start").click(function () {
         $("#chars").not('[clicked]').attr('clicked', '').append("<h3>Please choose your character.</h3>");
     }
 });
+
+// Object of different characters choosable for the game
 var chars = {
     boy: 'images/char-boy.png',
     catGirl: 'images/char-cat-girl.png',
     hornGirl: 'images/char-horn-girl.png',
     pinkGirl: 'images/char-pink-girl.png',
     princessGirl: 'images/char-princess-girl.png'
-}
+};
 var chooseChar;
-
-
-$("#chars img").click(function () {
-    $(this).addClass("clicked");
-    var _char = $(this).data("char");
-    if (_char === "boy") {
-        chooseChar = chars.boy;
-    } else if (_char === "cat-girl") {
-        chooseChar = chars.catGirl;
-    } else if (_char === "horn-girl") {
-        chooseChar = chars.hornGirl;
-    } else if (_char === "pink-girl") {
-        chooseChar = chars.pinkGirl;
-    } else {
-        chooseChar = chars.princessGirl;
-    }
-});
